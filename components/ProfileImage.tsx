d6228f23bd370ea7676e26881f9397138c2f945f
zaherlavi <lavizaher@gmail.com>
2025-01-17 01:44:16 +0100

    changed a photo link

--- components/ProfileImage.tsx
+++ components/ProfileImage.tsx
@@ -2,33 +2,29 @@
 import Image from "next/image";
 
 const ProfileImage = () => {
-    return (
-      <div className="relative flex justify-center lg:justify-end">
-        <div className="relative w-72 h-72 md:w-96 md:h-96">
-          {/* Decorative ring */}
-          <div className="absolute inset-0 rounded-full bg-gradient-to-r from-purple-600 to-purple-400 p-1">
-            <div className="absolute inset-0 rounded-full bg-black"></div>
-          </div>
-          
-          {/* Image container */}
-          <div className="relative w-full h-full rounded-full overflow-hidden">
-            <div className="absolute inset-0 flex items-center justify-center">
-              <img
-              src="https://c0yzrwzfg3.ufs.sh/f/HJYp9qKaAcIETKGUQa2ICpMmXAkQdi7SLwsh1uObrZ65NVxj"
-              alt="Zaher Lavi"
-              width={4583}
-              height={3429}
-              className="w-full h-full object-cover object-center"
-              />
-            </div>
-          </div>
-  
-          {/* Decorative elements */}
-          <div className="absolute -z-10 -top-4 -right-4 w-32 h-32 bg-purple-500/20 rounded-full blur-2xl" />
-          <div className="absolute -z-10 -bottom-4 -left-4 w-32 h-32 bg-purple-500/20 rounded-full blur-2xl" />
+  return (
+    <div className="relative flex justify-center lg:justify-end">
+      <div className="relative w-80 h-80 md:w-[28rem] md:h-[28rem]">
+        {/* Decorative ring */}
+        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-purple-600 to-purple-400 p-1">
+          <div className="absolute inset-0 rounded-full bg-black"></div>
         </div>
+
+        {/* Image container */}
+        <div className="relative w-full h-full rounded-full overflow-hidden border-2 border-purple-500/50">
+          <img
+            src="https://c0yzrwzfg3.ufs.sh/f/HJYp9qKaAcIEiPMLzuhS13gKhXWtaQ9uZkF7MxOe8cNA6nb0"
+            alt="Zaher Lavi"
+            className="w-full h-full object-cover object-center"
+          />
+        </div>
+
+        {/* Decorative elements */}
+        <div className="absolute -z-10 -top-4 -right-4 w-32 h-32 bg-purple-500/20 rounded-full blur-2xl" />
+        <div className="absolute -z-10 -bottom-4 -left-4 w-32 h-32 bg-purple-500/20 rounded-full blur-2xl" />
       </div>
-    );
-  };
-  
-  export default ProfileImage;
+    </div>
+  );
+};
+
+export default ProfileImage;
